32785b67c5a59f1cba866a95b605caa8e7a9a754
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-10-15 16:53:11 -0500

    comenzando con la navegacion

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+window.Vue = require('vue');
+console.log('hola')
+Vue.component('nav-component',require('./components/NavComponent.vue').default)
+const app = new Vue({
+    el : "#app"
+});
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,33 +9,32 @@
 </head>
 <body class="bg-gray-200">
     <div class="container mx-auto mt-5">
-        <div class="grid grid-cols-1 justify-center">
-            <div class="w-full max-w-xs">
-                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="flex justify-center">
+            <div class="w-full max-w-xs shadow-md rounded bg-white">
+                <h3 class="text-xl font-bold mb-2 mt-4  px-8 pt-2 ">Login</h3>
+                  <form method="POST" action="{{route('login')}}" class="bg-white  px-8 pt-6 pb-8 mb-4">
+                  @csrf
                   <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                      Username
+                      Email
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                    <input name="email" class="shadow @error('email') is_invalid @enderror appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Username">
                   </div>
                   <div class="mb-6">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                       Password
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                    <input name="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
                   </div>
                   <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                      Sign In
+                    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                      Ingresar
                     </button>
                     <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
                       Forgot Password?
                     </a>
                   </div>
                 </form>
-                <p class="text-center text-gray-500 text-xs">
-                  &copy;2020 Acme Corp. All rights reserved.
-                </p>
               </div>
         </div>
     </div>
