b099089ee6273b33ced3a0fd429db8477a750fd6
santishy <santiagomartinochoaestrada@gmail.com>
2020-11-30 19:24:49 -0600

    more cambios

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
     <div class="flex justify-center mt-20 items-baseline">
-        <category-search ></category-search>
-        <category-form ></category-form>
+        <category-search class="mr-4"></category-search>
+        <category-form class="mr-4"></category-form>
         <product-form method="post"></product-form>   
     </div>
 @endsection
