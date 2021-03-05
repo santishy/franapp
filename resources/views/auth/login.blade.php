685f0350616d45cbc64f224dfb7f5ba8ce86c1d7
santiago martin <santiagomartinochoaestrada@gmail.com>
2021-03-05 16:29:07 -0600

    login ...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,13 @@
                   <img class="w-1/3 h-auto" src="{{asset('images/logo.png')}}" alt="">
                 </div>
                 <h3 class="text-xl font-bold mb-0 mt-0  px-8 pt-2 text-center ">Ingresar al sistema</h3>
-                  <form method="POST" action="{{route('login')}}" class=" px-8 pt-6 pb-8 mb-4">
+                  <form method="POST" action="{{route('login')}}" class=" px-8 pt-0 pb-8 mb-4">
                   @csrf
-                  <div class="mb-0">
-                    <input name="email" class="border placeholder-gray-600 bg-gray-200 rounded relative rounded-bl-none rounded-br-none appearance-none  w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" id="username" type="email" placeholder="Email...">
+                  <div class="mb-0 px-2">
+                    <input name="email" class="border-2 placeholder-gray-600 bg-gray-100 rounded relative rounded-bl-none rounded-br-none appearance-none  w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10" id="username" type="email" placeholder="Email...">
                   </div>
-                  <div class="mb-0">
-                    <input name="password" class="border placeholder-gray-600 bg-gray-200 relative appearance-none rounded rounded-tl-none rounded-tr-none w-full py-3 px-3 text-gray-700  leading-tight focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 " id="password" type="password" placeholder="Contraseña...">
+                  <div class="mb-0 px-2">
+                    <input name="password" class="border-2 placeholder-gray-600 bg-gray-100 relative appearance-none rounded rounded-tl-none rounded-tr-none w-full py-3 px-3 text-gray-700  leading-tight focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 " id="password" type="password" placeholder="Contraseña...">
                   </div>
                   <div class="flex items-center justify-between mt-6">
                     <button class="  w-full bg-blue-500 hover:bg-blue-400 text-white font-bold py-3 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
