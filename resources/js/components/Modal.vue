ef7bc7b18bd346ad344d50435d0368db5620832a
santiago martin <santiagomartinochoaestrada@gmail.com>
2021-03-15 16:56:01 -0600

    ...

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -3,26 +3,37 @@
         <button @click="toggleModal = !toggleModal">
             <i class="far fa-window-maximize"></i>
         </button>
-        <div v-if="toggleModal" class="fixed overflow-x-hidden overflow-y-auto inset-0 flex justify-center items-center z-50">
-            <div class="relative mx-auto w-auto max-w-2xl">
-                <div class="bg-white w-full">
-                    hola mundo
+        <div
+            v-if="toggleModal"
+            class="fixed modal  overflow-x-hidden overflow-y-auto  flex justify-center items-center z-50"
+        >
+            <div class="relative mx-auto max-w-6xl w-full border border-red-600">
+                <div class="bg-white w-full rounded shadow-2xl p-4 relative">
+                  
                 </div>
             </div>
         </div>
+
         <div
-            v-if="toggleModal" 
-            class="absolute inset-0 opacity-25 bg-black z-40"
-        >
-        </div>
+            v-if="toggleModal"
+            @click="toggleModal = false"
+            class="fixed inset-0 opacity-25 bg-black z-40"
+        ></div>
     </div>
 </template>
 <script>
 export default {
-    data:()=>{
+    data: () => {
         return {
-            toggleModal:false
-        }
+            toggleModal: false
+        };
     }
 };
 </script>
+<style scoped>
+    .modal{
+        top:50%;
+        left:50%;
+        transform:translate(-50%,-50%);
+    }
+</style>
---
+++ resources/js/components/reports/products/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>SKU</th>
+                <th>Descripción</th>
+                <th>Precio</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr 
+                v-for="product in products"
+                :key=""
+            >
+
+            </tr>
+        </tbody>
+    </table>
+</template>
