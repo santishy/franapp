593193e80cbb89f72c13bb358b6232f1dbf10246
santishy <santiagomartinochoaestrada@gmail.com>
2021-03-22 19:14:38 -0600

    ...

--- resources/views/auth/roles/create.blade.php
+++ resources/views/auth/roles/create.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <div class="flex flex-wrap ">
+    <div class="flex flex-wrap items-baseline">
         <create-new-role></create-new-role>
         <permission-list 
+            class="ml-4"
             :permissions="{{json_encode($permissions)}}"
         >
         <permission-list>
