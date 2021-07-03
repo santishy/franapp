f087fe6cf74e92aee0af49137509521f0e2eb18a
santiago martin <santiagomartinochoaestrada@gmail.com>
2021-07-02 19:06:04 -0500

    ...

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,6 +3,5 @@
  <div class="mt-24 md:mt-32 md:w-1/3 w-full mx-auto bg-orange-400 rounded shadow-md p-8 flex fex-wrap justify-center items-center flex-col">
     <i class="fas fa-lock text-gray-700  text-6xl mb-4"></i>
     <h3 class="w-full text-3xl text-center text-gray-700">Acción no permitida</h3>
-     
  </div>   
 @endsection
