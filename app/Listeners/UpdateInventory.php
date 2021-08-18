2c263a79de92e24b29c1085d9f6fa09d7d1d89bf
santishy <santi_shy@hotmail.com>
2021-08-17 19:31:33 -0500

    corregir

--- app/Listeners/UpdateInventory.php
+++ app/Listeners/UpdateInventory.php
@@ -26,20 +26,23 @@ class UpdateInventory
      * @return void
      */
     public function handle(TransactionComplete $event)
-    {   
-    
-        if($event->transaction->status != 'completed')
+    {
+
+        if ($event->transaction->status != 'completed')
             return;
-        
+
         $inventory = Inventory::find(request('inventory_id'));
         $factor = $event->factor; // para sumar o restar segun se tenga que actualizar
 
-        $event->transaction->products()->get()->map(function ($product) use ($inventory,$factor) {
-            
+        $event->transaction->products()->get()->map(function ($product) use ($inventory, $factor) {
+
             $stock = $inventory->products()->where('inventory_product.product_id', $product->id);
-            
+
             if ($stock->exists()) {
-                
+                if ($factor == -1) {
+                    //dd('entro aki: '.$product->pivot->qty);
+                    $inventory->hasStock($product, $product->pivot->qty);
+                }
                 $inventory->products()->updateExistingPivot(
                     $stock->first()->id,
                     ['stock' => $stock->first()->pivot->stock + ($factor * $product->pivot->qty)]
