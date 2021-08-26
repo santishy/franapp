a6a4fcf4548296d3775b41b277bb608e7425db6f
santishy <santi_shy@hotmail.com>
2021-08-25 19:07:21 -0500

    ...

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sale;
+use App\Models\Ticket;
 use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
 
@@ -10,9 +11,9 @@ class PDFController extends Controller
 {
     function __invoke(Sale $sale)
     {
-        $products = $sale->products();
-        $size = array(0,0,302.36220472,null);
-        $pdf = PDF::loadView('tickets.pdf', compact('sale'));
+        $products = $sale->products;
+        $ticketConfig = Ticket::first();
+        $pdf = PDF::loadView('tickets.pdf', compact('sale','products','ticketConfig'));
         return $pdf->stream();
     }
 }
--- resources/views/tickets/pdf.blade.php
+++ resources/views/tickets/pdf.blade.php
@@ -5,12 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ticket</title>
-    <style>
-        @page{size:80mm auto}
-    </style>
 </head>
 <body>
-    <h1>Lorem ipsum dolor, sit amet consectetur
-         adipisicing elit. Consectetur architecto fugit ducimus aliquam dicta error quaerat asperiores minus. Dolorem, earum. Quod, ullam? Necessitatibus odit iste modi illum, fuga molestias praesentium?</h1>
+    <div>
+        <header>
+            {{$ticketConfig->header}}
+        </header>
+        <ul style="background: red;">
+            @foreach ($products as $product )
+                <li style="background:blue;display:flex; flex-wrap: wrap; justify-content:space-around">
+                    <div>{{$product->sku}}..........</div> 
+                    <div>4</div>
+                </li>    
+            @endforeach
+            
+        </ul>
+
+    </div>
 </body>
 </html>
