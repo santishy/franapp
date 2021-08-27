d80c2923533fef3c17047ac83689282581639b9e
santishy <santi_shy@hotmail.com>
2021-08-26 19:24:55 -0500

    ..

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -6,14 +6,20 @@ use App\Models\Sale;
 use App\Models\Ticket;
 use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PDFController extends Controller
 {
     function __invoke(Sale $sale)
     {
+        $now = Carbon::now()->format('Y-m-d');
         $products = $sale->products;
         $ticketConfig = Ticket::first();
-        $pdf = PDF::loadView('tickets.pdf', compact('sale','products','ticketConfig'));
+        $pdf = PDF::loadView(
+            'tickets.pdf',
+            compact('sale', 'now', 'products', 'ticketConfig')
+        )
+        ->setPaper('b7','portrait');
         return $pdf->stream();
     }
 }
--- resources/views/tickets/pdf.blade.php
+++ resources/views/tickets/pdf.blade.php
@@ -4,23 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ticket</title>
+    <title>Nota #{{$sale->id}}</title>
+    <style>
+        html, body {
+            margin:0%;
+            margin-right: 4pt;
+            margin-left: 4pt;
+        }
+        .text-center {
+            text-align: center;
+        }
+        .text-xl{
+            font-size: 18px;
+        }
+        .text-left{
+            text-align: left;
+        }
+        .text-right{
+            text-align: right;
+        }
+        .text-sm{
+            font-size: 0.7em;
+        }
+        .text-xs{
+            font-size: 0.5em;
+        }
+        .mt-2{
+            margin-top: 2em;
+        }
+        .mt-1{
+            margin-top: 1em;
+        }
+        .m-0{
+            margin:0%;
+        }
+    </style>
 </head>
 <body>
-    <div>
-        <header>
-            {{$ticketConfig->header}}
-        </header>
-        <ul style="background: red;">
-            @foreach ($products as $product )
-                <li style="background:blue;display:flex; flex-wrap: wrap; justify-content:space-around">
-                    <div>{{$product->sku}}..........</div> 
-                    <div>4</div>
-                </li>    
-            @endforeach
-            
-        </ul>
-
+    <div class="text-right text-sm mt-2">
+        {{$now}}
     </div>
+    <h1 class="text-xl text-center m-1">{{$ticketConfig->header}}</h1>
+    <div class="text-center">
+        Nota #{{$sale->id}}
+    </div>
+    <table class="mt-1">
+        <thead>
+            <tr>
+                <th>Producto</th>
+                <th>Cantidad</th>
+                <th>Subtotal</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
 </body>
 </html>
