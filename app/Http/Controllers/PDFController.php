8bc362f13c76e1c9fcbf59c5b4f09a52b52b6f8a
santiago martin <santiagomartinochoaestrada@gmail.com>
2021-09-02 13:42:47 -0500

    ...

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -16,27 +16,27 @@ class PDFController extends Controller
         $now = Carbon::now()->format('Y-m-d');
         $products = $sale->products;
         $ticketConfig = Ticket::first();
-        $height = '200mm';
+        
+        /** Se crea el frame de pdf la primera vez para calcular que tan grande sera el ticket, mando en altura 2000 como un maximo de altura sin que se rompa el codigo */
+        
         $pdf = PDF::loadView(
             'tickets.pdf',
-            compact('sale', 'now', 'products', 'ticketConfig', 'height')
-        );
-        $dom_pdf = $pdf->getDomPDF();
-        $dom_pdf->setCallbacks(
-            array(
-                'myCallbacks' => array(
-                    'event' => 'end_frame', 'f' => function ($infos) {
-                        $frame = $infos["frame"];
-                        if (strtolower($frame->get_node()->nodeName) === "body") {
-                            $padding_box = $frame->get_padding_box();
-                            dd($padding_box); // To see the output of the padding_box 
-                            $GLOBALS['bodyHeight'] += $padding_box['h'];
-                        }
-                    }
-                )
-            )
-        );
-        $pdf->setPaper(array(0, 0, 226.77, 841.89), 'portrait');
+            compact('sale', 'now', 'products', 'ticketConfig')
+        )->setPaper(array(0, 0, 227.67, 2000));
+        
+        /**
+         * Se obtiene la altura, la logica esta guardada en el modelo .. midiendo la altura total del body que esta dentro del padding al menos asi lo entendi
+         */
+        $height = $ticketConfig->getBodyHeight($pdf->getdomPDF());
+        
+        /**
+         * Se vuelve a mandar a crear el frame con la altura correspondiente al documento, de forma mas precisa y se manda pintar al final
+         */
+        $pdf = PDF::loadView(
+            'tickets.pdf',
+            compact('sale', 'now', 'products', 'ticketConfig')
+        )->setPaper(array(0, 0, 227.67, $height + 20));
+
         return $pdf->stream();
     }
 }
