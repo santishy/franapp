389b2c5fc0950698b212b0fd8b6c7e4b76002076
santishy <santi_shy@hotmail.com>
2021-09-28 19:18:26 -0500

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [
-    ".resources/js/components/**/*.vue", 
+  purge: [ 
   ],
   theme: {
     extend: {},
